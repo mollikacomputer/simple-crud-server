971bd595421da26e156941b149ffc9eb35ac2586
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-03-17 23:11:07 +0600

    undifine problem solve app.use express.json

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://mollikacomputer3:EhFlBrr6mzb57uML@cluster0.q5xegkn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,6 +24,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    app.post('/users', async(req, res)=>{
+      const user = await req.body;
+      console.log('New User', user);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
